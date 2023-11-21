f7012c7703dbb1e8dfdfa87e6aa5afcbb1e63a40
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2023-11-21 14:06:02 +0300

    Learning if/else/else if

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 fn main() {
-    let number = 3;
+    let a = 3;
 
-    if number < 5 {
+    if a < 5 {
         println!("condition was true");
     } else {
         println!("condition was false");
+    }
+    testif();
+}
+
+fn testif() {
+    let b = 5;
+
+    if b % 4 == 0 {
+        println!("Число делится на 4.");
+    } else if b % 3 == 0 {
+        println!("Число делится на 3");
+    } else if b % 2 == 0 {
+        println!("Число делится на 2.");
+    } else {
+        println!("Число не делится ни на 4, ни 3, ни на 2.");
     }
 }
